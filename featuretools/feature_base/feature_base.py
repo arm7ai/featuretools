78d53dc5d3114d316c3a077a8f03235984f38f64
Tamar Grey <tamar.grey@alteryx.com>
2020-09-03 16:25:19 -0400

    Change feature_base in arguments to be string

--- featuretools/feature_base/feature_base.py
+++ featuretools/feature_base/feature_base.py
@@ -807,7 +807,7 @@ class FeatureOutputSlice(FeatureBase):
     def get_arguments(self):
         return {
             'name': self._name,
-            'base_feature': self.base_feature,
+            'base_feature': self.base_feature.unique_name(),
             'n': self.n
         }
 
